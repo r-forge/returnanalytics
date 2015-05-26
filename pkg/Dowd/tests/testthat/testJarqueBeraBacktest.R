1593b4417b15bd1691b953d5267158aba34566fd
dacharya (Mr. ) <dines.acharya@gmail.com>
2015-05-26 19:53:32 +0000

    KSTestStat: source and documentation. KuiperTestStat: source and documentation. JarqueBeraBacktest: source, documentation and test.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/returnanalytics@3650 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/Dowd/tests/testthat/testJarqueBeraBacktest.R
@@ -0,0 +1,5 @@
+test_that("Binomial Backtest Works.",{
+  # Success
+  expect_equal(0.6942, JarqueBeraBacktest(-0.0758, 2.8888, 500), tolerance=0.01)
+  expect_equal(1, JarqueBeraBacktest(0, 3, 100), tolerance=0.01)
+})
