58f7f289a50a76378eac45c8c3939d22bab5e19b
dacharya <dacharya@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-06-09 21:12:47 +0000

    MEFPlot and NormalQQPlot: source and documentation.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/returnanalytics@3666 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/Dowd/R/MEFPlot.R
@@ -0,0 +1,33 @@
+#' Mean Excess Function Plot
+#'
+#' Plots mean-excess function values of the data set.
+#'
+#' @param Ra Vector data
+#' 
+#' @references Dowd, K. Measuring Market Risk, Wiley, 2007.
+#' 
+#' @author Dinesh Acharya
+#' @examples
+#' 
+#'    # Plots 
+#'    Ra <- rnorm(1000)
+#'    MEFPlot(Ra)
+#'
+#' @export
+MEFPlot <- function(Ra){
+  data <- as.vector(Ra)
+  if (!is.vector(data)) {
+    stop("Input should be a vector data.")
+  }
+  u <- data
+  n <- length(u)
+  mef <- 
+  for (i in 1:n - 1) {
+    data <- data[which(data > u[i])]
+    mef[i] <- mean(data) - u[i]
+  }
+  u <- u[!u==max(u)]
+  plot(u, mef, type = "l", xlab = "Threshold", ylab = "e(u)", 
+       main = "Empirical Mean Excess Function")
+  
+} 
