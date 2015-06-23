4b3da007a9ab5041167015c2775f7f5b221395b8
dacharya (Mr. ) <dines.acharya@gmail.com>
2015-06-23 20:01:39 +0000

    More test cases added.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/returnanalytics@3723 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Dowd/tests/testthat/testBinomialBacktest.R
+++ pkg/Dowd/tests/testthat/testBinomialBacktest.R
@@ -1,11 +1,19 @@
 test_that("Binomial Backtest Works.",{
-  # Success
+  # Success - 1
+  # x < n * p
   expect_equal(0.7358, BinomialBacktest(1, 100, 0.99), tolerance=0.001)
-  expect_equal(0.2529, BinomialBacktest(55, 1000, 0.95), tolerance=0.001)
   
-  # Warnings
-  expect_warning(val <- BinomialBacktest(35,30,0.95))
-  expect_true(is.nan(val))
-  expect_warning(val <- BinomialBacktest(5,30,1.5))
-  expect_true(is.nan(val))
+  # Success - 2
+  # x >= n * p
+  expect_equal(0, BinomialBacktest(91, 100, 0.90), tolerance=0.001)
+  
+  # Error - 1
+  expect_error(val <- BinomialBacktest(35, 30, 0.95))
+  
+  # Error - 2
+  expect_error(val <- BinomialBacktest(5, 30, 1.5))
+  
+  # Error - 3
+  expect_error(val <- BinomialBacktest(5, 30, -.95))
+
 })
