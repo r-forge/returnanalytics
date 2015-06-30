4548e94b76b8095a98027430471a6ff877b9bb58
dacharya (Mr. ) <dines.acharya@gmail.com>
2015-06-30 08:47:19 +0000

    KernelESTriangleKernel function added.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/returnanalytics@3764 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/Dowd/R/KernelESTriangleKernel.R
@@ -0,0 +1,29 @@
+#' Calculates ES using triangle kernel approach
+#' 
+#' The output consists of a scalar ES for specified confidence level.
+#' 
+#' @param Ra Profit and Loss data set
+#' @param cl VaR confidence level
+#' @return Scalar VaR
+#' @references Dowd, K. Measuring Market Risk, Wiley, 2007.
+#'
+#' @author Dinesh Acharya
+#' @examples
+#' 
+#'    # VaR for specified confidence level using triangle kernel approach
+#'    Ra <- rnorm(30)
+#'    KernelESTriangleKernel(Ra, .95)
+#'
+#' @export
+KernelESTriangleKernel <- function(Ra, cl){
+  PandL <- as.vector(Ra)
+  n <- 1000
+  delta.cl <- (1 - cl) / n
+  VaR <- double(999)
+  for (i in 1:(n - 1)) {
+    VaR[i] <- KernelVaRTriangleKernel(PandL, cl + i * delta.cl)
+  }
+  ES <- mean(VaR)
+  return(ES)
+  
+}
