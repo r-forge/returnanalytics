0e23db3cb6810c517151f49791b29856741d9e1f
dacharya <dacharya@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-07-28 23:12:58 +0000

    Small type in function name corrected.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/returnanalytics@3876 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Dowd/R/AmericanPutVaRBinomial.R
+++ pkg/Dowd/R/AmericanPutVaRBinomial.R
@@ -23,10 +23,10 @@
 #' @examples
 #' 
 #'    # Market Risk of American Put with given parameters.
-#'    AmericanPutVarBinomial(0.20, 27.2, 25, .16, .05, 60, 20, .95, 30)
+#'    AmericanPutVaRBinomial(0.20, 27.2, 25, .16, .05, 60, 20, .95, 30)
 #' 
 #' @export
-AmericanPutVarBinomial <- function(amountInvested, stockPrice, strike, r,
+AmericanPutVaRBinomial <- function(amountInvested, stockPrice, strike, r,
                                    volatility, maturity, numberSteps, cl, hp){
   s <- stockPrice
   x <- strike
