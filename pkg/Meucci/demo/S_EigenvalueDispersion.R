ad7319dd2012a744464893ad84f33ea2d6e75d2b
xavierv <xavierv@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-08-13 16:11:51 +0000

    Formatted demo scripts and relating functions up to S_D*
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/returnanalytics@3953 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Meucci/demo/S_EigenvalueDispersion.R
+++ pkg/Meucci/demo/S_EigenvalueDispersion.R
@@ -1,60 +1,60 @@
-#' This script displays the sample eigenvalues dispersion phenomenon, as described in A. Meucci,
-#' "Risk and Asset Allocation", Springer, 2005,  Chapter 4.
+#' This script displays the sample eigenvalues dispersion phenomenon,
+#' as described in A. Meucci, "Risk and Asset Allocation", Springer, 2005, 
+#' Chapter 4.
 #'
 #' @references
-#' A. Meucci - "Exercises in Advanced Risk and Portfolio Management" \url{http://symmys.com/node/170},
-#' "E 168 - Sample covariance and eigenvalue dispersion".
+#' A. Meucci - "Exercises in Advanced Risk and Portfolio Management"
+#' \url{http://symmys.com/node/170}, "E 168 - Sample covariance and eigenvalue
+#' dispersion".
 #'
 #' See Meucci's script for "S_EigenValueDispersion.m"
 #'
 #' @author Xavier Valls \email{xaviervallspla@@gmail.com}
 
-##################################################################################################################
+################################################################################
 ### Inputs
 
-N    = 50;
-nSim = 50;
-SampleLenght = seq( N , 10 * N, N)
+N    <- 50
+nSim <- 50
+SampleLenght <- seq(N, 10 * N, N)
 
-##################################################################################################################
+################################################################################
 ### Generate mesh and surface
 
-Mu    = matrix( 0, N, 1 );
-Sigma = diag( 1, N );
+Mu    <- matrix(0, N, 1)
+Sigma <- diag(1, N)
 
 # compute true eigenvalues
-Eigen = eigen(Sigma);
-Index = order( -( Eigen$values ));
-EVec  = Eigen$vectors[ , Index ];
-EVal  = diag( Eigen$Values[ Index, Index ]);
+Eigen <- eigen(Sigma)
+Index <- order(-(Eigen$values))
+EVec  <- Eigen$vectors[, Index]
+EVal  <- diag(Eigen$Values[Index, Index])
 
 # compute eigenvalues of sample estimator
-nSampleLenght  = length( SampleLenght );
-Store_EVal_Hat = matrix( NaN, nSampleLenght, N ); # preallocation for speed
-for( i in 1 : nSampleLenght )
- {  
-    T = SampleLenght[ i ];
-    EVal_Hat = 0;
-    for( n in 1 : nSim )
-    {
-        X = rmvnorm( T, Mu, Sigma );
-        Sigma_Hat = cov( X );
-        L = eigen( Sigma_Hat )$values;
-        Index = order(-(L));
-        L = L[ Index];
+nSampleLenght  <- length(SampleLenght)
+Store_EVal_Hat <- matrix(NaN, nSampleLenght, N) # preallocation for speed
+for (i in 1 : nSampleLenght) {
+    T <- SampleLenght[i]
+    EVal_Hat <- 0
+    for (n in 1 : nSim) {
+        X <- rmvnorm(T, Mu, Sigma)
+        Sigma_Hat <- cov(X)
+        L <- eigen(Sigma_Hat)$values
+        Index <- order(-(L))
+        L <- L[Index]
 
-        EVal_Hat = EVal_Hat + L;
+        EVal_Hat <- EVal_Hat + L
     }
-    EVal_Hat = EVal_Hat / nSim;
+    EVal_Hat <- EVal_Hat / nSim
 
-    Store_EVal_Hat[ i, ] = t(EVal_Hat);
+    Store_EVal_Hat[i, ] <- t(EVal_Hat)
 }
 
-##################################################################################################################
+################################################################################
 ### Display surface
-dev.new();
+dev.new()
 
-persp( SampleLenght/N, 1 :N , Store_EVal_Hat,
-    theta = 7 * 45, phi = 30, expand=0.6, col='lightblue', shade=0.75, ltheta=120, 
-    ticktype='detailed', xlab = "eigenvalue #", ylab = "sample lenght/N");
-
+persp(SampleLenght/N, 1 :N, Store_EVal_Hat,
+    theta = 7 * 45, phi = 30, expand=0.6, col='lightblue', shade=0.75,
+    ltheta=120, ticktype='detailed', xlab = "eigenvalue #",
+    ylab = "sample lenght/N")
