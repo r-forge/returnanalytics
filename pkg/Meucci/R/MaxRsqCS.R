1f2a137e445135532d551fe42c7d5fff6af35a80
xavierv (Xavier Valls) <flamejat@gmail.com>
2015-08-13 16:11:51 +0000

    Formatted demo scripts and relating functions up to S_D*
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/returnanalytics@3953 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Meucci/R/MaxRsqCS.R
+++ pkg/Meucci/R/MaxRsqCS.R
@@ -1,9 +1,9 @@
-#' @title Solve for G that maximises sample r-square of X*G'*B' with X under constraints A*G<=D
-#' and Aeq*G=Deq
+#' @title Solve for G that maximises sample r-square of X*G'*B' with X under
+#' constraints A*G<=D and Aeq*G=Deq
 #'
-#' @description Solve for G that maximises sample r-square of X*G'*B' with X under constraints A*G<=D
-#' and Aeq*G=Deq (A,D, Aeq,Deq conformable matrices),as described in  A. Meucci, 
-#' "Risk and Asset Allocation", Springer, 2005.
+#' @description Solve for G that maximises sample r-square of X*G'*B' with X
+#' under constraints A*G<=D and Aeq*G=Deq (A,D, Aeq,Deq conformable matrices),
+#' as described in  A. Meucci, "Risk and Asset Allocation", Springer, 2005.
 #'  
 #'  @param   X   : [matrix] (T x N)
 #'  @param   B   : [matrix] (T x K)
@@ -17,105 +17,94 @@
 #'  
 #'  @return   G   : [matrix] (N x K)
 #'
-#'  @note
-#'  Initial code by Tai-Ho Wang 
+#' @note
+#' Initial code by Tai-Ho Wang 
 #'
 #' @references
-#' A. Meucci - "Exercises in Advanced Risk and Portfolio Management" \url{http://symmys.com/node/170}.
-#' Used in "E 123 - Cross-section factors: generalized cross-section industry factors".
-#'
+#' A. Meucci - "Exercises in Advanced Risk and Portfolio Management"
+#' \url{http://symmys.com/node/170}. Used in "E 123 - Cross-section factors:
+#' generalized cross-section industry factors".
 #' See Meucci's script for "MaxRsqCS.m"
 #'
 #' @author Xavier Valls \email{xaviervallspla@@gmail.com}
 #' @export
 
-MaxRsqCS = function(X, B, W, A = NULL, D = NULL, Aeq = NULL, Deq, lb = NULL, ub = NULL)
-{
-    N = ncol(X);
-    K = ncol(B);
+MaxRsqCS <- function(X, B, W, A = NULL, D = NULL, Aeq = NULL, Deq, lb = NULL,
+                    ub = NULL) {
+    N <- ncol(X)
+    K <- ncol(B)
 
     # compute sample estimates
-    Sigma_X = (dim(X)[1]-1)/dim(X)[1] * cov(X);
+    Sigma_X <- (dim(X)[1] - 1) / dim(X)[1] * cov(X)
 
 
-    # restructure for feeding to quadprog 
-    Phi = t(W) %*% W;
+    # restructure for feeding to quadprog
+    Phi <- t(W) %*% W
 
-    # restructure the linear term of the objective function 
-    FirstDegree = matrix( Sigma_X %*% Phi %*% B, K * N, );
+    # restructure the linear term of the objective function
+    FirstDegree <- matrix(Sigma_X %*% Phi %*% B, K * N, )
 
     # restructure the quadratic term of the objective function
-    SecondDegree = Sigma_X;
-    
-    for( k in 1 : (N - 1) )
-    {
-        SecondDegree = blkdiag(SecondDegree, Sigma_X);
-    }
-    
-    SecondDegree = t( kron( sqrt(Phi) %*% B, diag( 1, N ) ) ) %*% SecondDegree %*% kron( sqrt( Phi ) %*% B, diag( 1, N ) );
+    SecondDegree <- Sigma_X
 
-    # restructure the equality constraints 
-    if( !length(Aeq)  )
-    {
-        AEq = Aeq;
-    }else
-    {
-        AEq = blkdiag(Aeq);
-        for( k in 2 : K )
-        {
-            AEq = blkdiag(AEq, Aeq);
+    for (k in 1 : (N - 1)) {
+        SecondDegree <- blkdiag(SecondDegree, Sigma_X)
+    }
+
+    SecondDegree <- t(kron(sqrt(Phi) %*% B, diag(1, N))) %*% SecondDegree %*%
+                    kron(sqrt(Phi) %*% B, diag(1, N))
+
+    # restructure the equality constraints
+    if (!length(Aeq) ) {
+        AEq <- Aeq
+    } else {
+        AEq <- blkdiag(Aeq)
+        for (k in 2 : K) {
+            AEq <- blkdiag(AEq, Aeq)
         }
     }
 
-    Deq = matrix( Deq, , 1);
+    Deq <- matrix(Deq, , 1)
 
-    # resturcture the inequality constraints 
-    if( length(A) )
-    {
-        AA = NULL
-        for( k in 1 : N )
-        {
-            AA = cbind( AA, kron(diag( 1, K ), A[ k ] ) ); ##ok<AGROW>
+    # resturcture the inequality constraints
+    if(length(A)) {
+        AA <- NULL
+        for (k in 1 : N) {
+            AA <- cbind(AA, kron(diag(1, K), A[k]))
         }
-    }else
-    {
-         AA = A;
+    } else {
+         AA <- A
     }
 
-    if( length(D))
-    {
-        D = matrix( D, , 1 );
+    if (length(D)) {
+        D <- matrix(D, , 1)
     }
 
     # restructure upper and lower bounds
-    if( length(lb) )
-    {
-        lb =  matrix( lb, K * N, 1 );
+    if (length(lb)) {
+        lb <-  matrix(lb, K * N, 1)
     }
 
-    if( length(ub) )
-    {
-        ub = matrix( ub, K * N, 1 );
+    if (length(ub)) {
+        ub <- matrix(ub, K * N, 1)
     }
 
     # initial guess
-    x0 = matrix( 1, K * N, 1 );
-    if(length(AA))
-    {
-        AA = ( AA + t(AA) ) / 2; # robustify
-          
+    x0 <- matrix(1, K * N, 1)
+    if(length(AA)) {
+        AA <- (AA + t(AA)) / 2 # robustify
     }
 
-    Amat = rbind( AEq, AA);
-    bvec = c( Deq, D  );
-    
+    Amat <- rbind(AEq, AA)
+    bvec <- c(Deq, D)
+
     # solve the constrained generlized r-square problem by quadprog
-    #options = optimset('LargeScale', 'off', 'MaxIter', 2000, 'Display', 'none');
-    
+    #options = optimset('LargeScale', 'off', 'MaxIter', 2000, 'Display', 'none')
 
-    b = ipop( c = matrix( FirstDegree ), H = SecondDegree, A = Amat, b = bvec, l = lb , u = ub , r = rep(0, length(bvec)) )
+    b <- ipop(c = matrix(FirstDegree), H = SecondDegree, A = Amat, b = bvec,
+              l = lb, u = ub, r = rep(0, length(bvec)))
     # reshape for output
-    G = t( matrix( attributes(b)$primal, N, ) );
+    G <- t(matrix(attributes(b)$primal, N, ))
 
-    return( G );
+    return(G)
 }
--- pkg/Meucci/R/MaxRsqTS.R
+++ pkg/Meucci/R/MaxRsqTS.R
@@ -1,6 +1,6 @@
-#' Solve for B that maximises sample r-square of F'*B' with X under constraints A*G<=D
-#' and Aeq*G=Deq (A,D, Aeq,Deq conformable matrices),as described in  A. Meucci, 
-#' "Risk and Asset Allocation", Springer, 2005.
+#' Solve for B that maximises sample r-square of F'*B' with X under constraints
+#' A*G<=D and Aeq*G=Deq (A,D, Aeq,Deq conformable matrices),as described in
+#'  A. Meucci, "Risk and Asset Allocation", Springer, 2005.
 #'  
 #'  @param   X   : [matrix] (T x N)
 #'  @param   F   : [matrix] (T x K)
@@ -18,109 +18,98 @@
 #'  Initial MATLAB's code by Tai-Ho Wang. 
 #'
 #' @references
-#' A. Meucci - "Exercises in Advanced Risk and Portfolio Management" \url{http://symmys.com/node/170}.
-#' See Meucci's script for "MaxRsqTS.m"
+#' A. Meucci - "Exercises in Advanced Risk and Portfolio Management"
+#' \url{http://symmys.com/node/170}. See Meucci's script for "MaxRsqTS.m"
 #'
 #' @author Xavier Valls \email{xaviervallspla@@gmail.com}
 #' @export
 
-MaxRsqTS = function(X, F, W, A = NULL, D = NULL, Aeq = NULL, Deq, lb = NULL, ub = NULL)
-{
+MaxRsqTS <- function(X, F, W, A = NULL, D = NULL, Aeq = NULL, Deq, lb = NULL,
+                     ub = NULL) {
 
-	N = dim(X)[ 2 ];
-	K = dim(F)[ 2 ];
-    X = matrix(as.numeric(X), dim(X))
+	N <- dim(X)[2]
+	K <- dim(F)[2]
+    X <- matrix(as.numeric(X), dim(X))
 
 
 	# compute sample estimates
-	# E_X = apply( X, 2, mean);
-	# E_F = apply( F, 2, mean);
-	XF = cbind( X, F );
-	SigmaJoint_XF = (dim(XF)[1]-1)/dim(XF)[1] * cov(XF);
-	
-	Sigma_X  = SigmaJoint_XF[ 1:N, 1:N ];
-	Sigma_XF = SigmaJoint_XF[ 1:N, (N+1):(N+K) ];
-	Sigma_F  = SigmaJoint_XF[ (N+1):(N+K), (N+1):(N+K) ];
+	# E_X = apply(X, 2, mean)
+	# E_F = apply(F, 2, mean)
+	XF <- cbind(X, F)
+	SigmaJoint_XF <- (dim(XF)[1] - 1) / dim(XF)[1] * cov(XF)
+
+	Sigma_X  <- SigmaJoint_XF[1:N, 1:N]
+	Sigma_XF <- SigmaJoint_XF[1:N, (N + 1):(N + K)]
+	Sigma_F  <- SigmaJoint_XF[(N + 1):(N + K), (N + 1):(N + K)]
 
 
-    # restructure for feeding to quadprog 
-    Phi = t(W) %*% W;
-    trSigma_WX = sum( diag( Sigma_X %*% Phi ) );
+    # restructure for feeding to quadprog
+    Phi <- t(W) %*% W
+    trSigma_WX <- sum(diag(Sigma_X %*% Phi))
 
-    # restructure the linear term of the objective function 
-    FirstDegree = ( -1 / trSigma_WX ) * matrix( t( Phi %*% Sigma_XF ), N*K, );
+    # restructure the linear term of the objective function
+    FirstDegree <- (-1 / trSigma_WX) * matrix(t(Phi %*% Sigma_XF), N * K,)
 
     # restructure the quadratic term of the objective function
-    SecondDegree = Sigma_F;
-    for( k in 1 : (N - 1) )
-    {
-        SecondDegree = blkdiag( SecondDegree, Sigma_F );
+    SecondDegree <- Sigma_F
+    for(k in 1 : (N - 1)) {
+        SecondDegree <- blkdiag(SecondDegree, Sigma_F)
     }
-    SecondDegree = ( 1 / trSigma_WX ) * t(kron( sqrt( Phi ), diag( 1, K ))) %*% SecondDegree %*% kron( sqrt(Phi), diag( 1, K ) );
+    SecondDegree <- (1 / trSigma_WX) * t(kron(sqrt(Phi), diag(1, K))) %*%
+                    SecondDegree %*% kron(sqrt(Phi), diag(1, K))
 
-    # restructure the equality constraints 
-    if( !length(Aeq)  )
-    {
-        AEq = Aeq;
-    }else
-    {
-        AEq = NULL;
-        for( k in 1 : N )
-        {
-            AEq = cbind( AEq, kron( diag( 1, K ), Aeq[k] ) );
+    # restructure the equality constraints
+    if(!length(Aeq) ) {
+        AEq <- Aeq
+    } else {
+        AEq <- NULL
+        for (k in 1 : N) {
+            AEq <- cbind(AEq, kron(diag(1, K), Aeq[k]))
         }
     }
 
-    Deq = matrix( Deq, , 1);
+    Deq <- matrix(Deq, , 1)
 
-    # resturcture the inequality constraints 
-    if( length(A) )
-    {
-        AA = NULL
-        for( k in 1 : N )
-        {
-            AA = cbind( AA, kron(diag( 1, K ), A[ k ] ) ); ##ok<AGROW>
+    # resturcture the inequality constraints
+    if(length(A)) {
+        AA <- NULL
+        for (k in 1 : N) {
+            AA <- cbind(AA, kron(diag(1, K), A[k]))
         }
-    }else
-    {
-         AA = A;
+    } else {
+         AA <- A
     }
 
-    if( length(D))
-    {
-        D = matrix( D, , 1 );
+    if(length(D)) {
+        D <- matrix(D, , 1)
     }
 
     # restructure upper and lower bounds
-    if( length(lb) )
-    {
-        lb =  matrix( lb, K * N, 1 );
+    if(length(lb)) {
+        lb <-  matrix(lb, K * N, 1)
     }
 
-    if( length(ub) )
-    {
-        ub = matrix( ub, K * N, 1 );
+    if(length(ub)) {
+        ub <- matrix(ub, K * N, 1)
     }
 
     # initial guess
-    x0 = matrix( 1, K * N, 1 );
-    if(length(AA))
-    {
-        AA = ( AA + t(AA) ) / 2; # robustify
-          
+    x0 <- matrix(1, K * N, 1)
+    if(length(AA)) {
+        AA <- (AA + t(AA)) / 2 # robustify
     }
 
-    Amat = rbind( AEq, AA);
-    bvec = c( Deq, D  );
+    Amat <- rbind(AEq, AA)
+    bvec <- c(Deq, D )
 
     # solve the constrained generlized r-square problem by quadprog
-    #options = optimset('LargeScale', 'off', 'MaxIter', 2000, 'Display', 'none');
-    
+    #options = optimset('LargeScale', 'off', 'MaxIter', 2000, 'Display', 'none')
 
-    b = ipop( c = matrix( FirstDegree ), H = SecondDegree, A = Amat, b = bvec, l = lb , u = ub , r = rep(0, length(bvec)) )
-    
+    b <- ipop(c = matrix(FirstDegree), H = SecondDegree, A = Amat, b = bvec,
+              l = lb, u = ub, r = rep(0, length(bvec)))
+
     # reshape for output
-    G = matrix( attributes(b)$primal, N, ) ;
+    G <- matrix(attributes(b)$primal, N,)
 
-    return( G );
+    return(G)
 }
